32d5fffba8bb6773fb1b0b57a6831539fa320ffb
zsarge <zackarywsargent+github@gmail.com>
2021-11-05 13:05:41 -0400

    enforce linux kernel style guide

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -6,24 +6,24 @@
 #include <stdlib.h>
 
 struct Node {
-	struct Node* next;
+	struct Node *next;
 	int value;
 };
 
 struct LinkedList {
-	struct Node* head;
+	struct Node *head;
 };
 
-struct LinkedList* make_list(void) {
-	struct LinkedList* list = malloc(sizeof(struct LinkedList));
+struct LinkedList *make_list(void) {
+	struct LinkedList *list = malloc(sizeof(struct LinkedList));
 	if (!list)
 		return NULL;
 	list->head = NULL;
 	return list;
 }
 
-struct Node* create_node(int value) {
-	struct Node* node = malloc(sizeof(struct Node));
+struct Node *create_node(int value) {
+	struct Node *node = malloc(sizeof(struct Node));
 	if (!node)
 		return NULL;
 	node->next = NULL;
@@ -42,23 +42,24 @@ void display_with_index(struct Node node, int index) {
 		display_with_index(*node.next, index + 1);
 }
 
-void push_back(struct Node* node, struct LinkedList* list) {
+void push_back(struct Node *node, struct LinkedList *list) {
 	if (list->head == NULL) {
 		list->head = node;
 		return;
 	}
-	struct Node* current = list->head;
-	for (; current->next != NULL; current = current->next);
+	struct Node *current = list->head;
+	while (current->next != NULL) 
+		current = current->next;
 	current->next = node;
 }
 
-void pop_back(struct LinkedList* list) {
+void pop_back(struct LinkedList *list) {
 	if (list->head == NULL) {
 		return;
 	} 
-	struct Node* current = list->head;
-	struct Node* previous = current;
-	while(current->next != NULL) {
+	struct Node *current = list->head;
+	struct Node *previous = current;
+	while (current->next != NULL) {
 		previous = current;
 		current = current->next;
 	}
@@ -66,20 +67,20 @@ void pop_back(struct LinkedList* list) {
 	free(previous->next);
 }
 
-void display(struct Node* node) {
+void display(struct Node *node) {
 	display_with_index(*node, 0);
 }
 
-void display_list(struct LinkedList* list) {
+void display_list(struct LinkedList *list) {
 	int index = 0;
 	printf("Displaying List:\n");
-	for (struct Node* current = list->head; current != NULL; current = current->next) {
+	for (struct Node *current = list->head; current != NULL; current = current->next) {
 		printf("%d = %d\n", index++, current->value);
 	}
 }
 
-void delete_list(struct LinkedList* list) {
-	for (struct Node* current = list->head; current != NULL; current = current->next) {
+void delete_list(struct LinkedList *list) {
+	for (struct Node *current = list->head; current != NULL; current = current->next) {
 		free(current);
 	}
 	free(list);
@@ -87,7 +88,7 @@ void delete_list(struct LinkedList* list) {
 
 int main(void) {
 	// construct list
-	struct LinkedList* list = make_list();
+	struct LinkedList *list = make_list();
 
 	// add data
 	push_back(create_node(10), list);
