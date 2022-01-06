9abb13303805e61765ef28480a6a4abb76cbe756
zsarge <zackarywsargent+github@gmail.com>
2022-01-05 21:16:08 -0500

    clean up solution

--- recursive-example/hs/main.hs
+++ recursive-example/hs/main.hs
@@ -1,26 +1,23 @@
+module Main where
 import Text.Printf
 
-findSolution target = formatSolution
-  where
-    formatSolution :: String
-    formatSolution =
-      case find 1 "1" of
-        Just a -> a
-        Nothing -> "no solution"
-    find :: Integer -> String -> Maybe String
-    find x acc
-      | x == target = Just acc
-      | x > target  = Nothing
-      | otherwise   = getFirstValid
-      where
-        getFirstValid =
-          case (plus5, times3) of
-            (Just x, _) -> Just x
-            (_, Just x) -> Just x
-            (Nothing, Nothing) -> Nothing
-          where
-            plus5  = (find (x + 5) (printf "(%s + 5)" acc))
-            times3 = (find (x * 3) (printf "(%s * 3)" acc))
+findSolution :: Integer -> String
+findSolution target =
+    case (find 1 "1") of
+       Just a -> a
+       Nothing -> "no solution"
+    where
+       find :: Integer -> String -> Maybe String
+       find x acc
+         | x == target = Just acc
+         | x > target  = Nothing
+         | otherwise   = case (plus5, times3) of
+                           (Just a, _) -> Just a
+                           (_, Just a) -> Just a
+                           (Nothing, Nothing) -> Nothing
+                         where
+                           plus5  = (find (x + 5) (printf "(%s + 5)" acc))
+                           times3 = (find (x * 3) (printf "(%s * 3)" acc))
 
 main :: IO ()
 main = putStrLn $ findSolution 253
